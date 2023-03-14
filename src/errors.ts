1b1acf3cf56db305c23f31d96910dd2fc50e51a6
achingbrain <alex@achingbrain.net>
2023-03-14 14:09:10 +0100

    fix: add error for get failed
    For when getting a block fails, rather than when it wasn't found

--- src/errors.ts
+++ src/errors.ts
@@ -15,6 +15,11 @@ export function putFailedError (err?: Error): Error {
   return errCode(err, 'ERR_PUT_FAILED')
 }
 
+export function getFailedError (err?: Error): Error {
+  err = err ?? new Error('Get failed')
+  return errCode(err, 'ERR_GET_FAILED')
+}
+
 export function deleteFailedError (err?: Error): Error {
   err = err ?? new Error('Delete failed')
   return errCode(err, 'ERR_DELETE_FAILED')
